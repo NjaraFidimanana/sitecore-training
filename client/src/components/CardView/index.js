4abc364611a78f1e96cc939f2e8bdd149a3850ea
Njara <njara.fidimanana@delaware.pro>
2021-04-17 00:03:33 +0200

    [Sitecore][React] Search page result with connected graphQL

--- client/src/components/CardView/index.js
+++ client/src/components/CardView/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { RichText, Text, Image } from '@sitecore-jss/sitecore-jss-react';
-import './CardView.module.css';
-const CardView = (props) => (
-  <div className="card-container">
-        <li className="box-list-item box-logo-button-item">
+import './CardView.css';
+/* Static resolution for images */
+const CardView = (props) => {
+  console.log('Image',props.image)
+  return (<div className="card-container Card">
+        <div className="box-list-item box-logo-button-item">
             <a href={props.url}>
                 <div className="Logo">
-                <Image field={props.image} className="ImgCard" width="300px" height="150px" />
+                { props.image && <Image field={props.image.jss} className="ImgCard" width="50%" height="50%" />}
                 </div>
                 <div className="content">
                   <Text tag="h6" field={props.title} />
@@ -14,7 +16,7 @@ const CardView = (props) => (
                 </div>
             </a>
 
-	    </li>
-  </div>
-);
+	    </div>
+  </div>)
+};
 export default CardView;
